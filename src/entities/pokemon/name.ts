f9087eb86dec95e1e2f3920934c897e59061007c
wilson Neto <wngaspar10@gmail.com>
2023-02-02 09:47:24 -0300

    feat: added name object value

---
+++ src/entities/pokemon/name.ts
@@ -0,0 +1,30 @@
+import { InvalidNameError } from '@/entities/errors'
+import { Either, left, right } from '@/shared'
+
+export class Name {
+  public readonly value: string
+
+  private constructor (name: string) {
+    this.value = name
+  }
+
+  static create (name: string): Either<InvalidNameError, Name> {
+    if (!Name.validate(name)) {
+      return left(new InvalidNameError(name))
+    }
+
+    return right(new Name(name))
+  }
+
+  public static validate (name: string): boolean {
+    if (!name) {
+      return false
+    }
+
+    if (name.trim().length < 2 || name.trim().length > 120) {
+      return false
+    }
+
+    return true
+  }
+}
