ee9838957728be6496a6cfd0eb08bf652d348a20
wilson Neto <wngaspar10@gmail.com>
2023-02-02 12:46:59 -0300

    feat: create http helper

---
+++ src/web-controller/util/http-helper.ts
@@ -0,0 +1,21 @@
+import { HttpResponse } from '@/web-controller/ports'
+
+export const ok = (data: any): HttpResponse => ({
+  statusCode: 200,
+  body: data
+})
+
+export const created = (data: any): HttpResponse => ({
+  statusCode: 201,
+  body: data
+})
+
+export const badRequest = (data: any): HttpResponse => ({
+  statusCode: 400,
+  body: data
+})
+
+export const serverError = (data: any): HttpResponse => ({
+  statusCode: 500,
+  body: data
+})
