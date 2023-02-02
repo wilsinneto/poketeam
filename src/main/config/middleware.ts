213f10d0200249dc443df3be80b7f35660afb7b6
wilson Neto <wngaspar10@gmail.com>
2023-02-02 14:36:31 -0300

    feat: added cors config

--- src/main/config/middleware.ts
+++ src/main/config/middleware.ts
@@ -1,6 +1,8 @@
 import { bodyParser } from '@/main/middleware'
+import { cors } from '@/main/middleware/cors'
 import { Express } from 'express'
 
 export default (app: Express): void => {
   app.use(bodyParser)
+  app.use(cors)
 }
