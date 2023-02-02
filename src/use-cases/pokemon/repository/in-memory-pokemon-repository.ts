25954e58ea17391e7d997c4f8fa87ab65e3f3fa3
wilson Neto <wngaspar10@gmail.com>
2023-02-02 11:14:21 -0300

    feat: user in memory pokemon repository

---
+++ src/use-cases/pokemon/repository/in-memory-pokemon-repository.ts
@@ -0,0 +1,38 @@
+import { PokemonDTO } from '@/entities/pokemon'
+import { PokemonRepository } from '@/use-cases/pokemon/ports'
+
+export class InMemoryPokemonRepository implements PokemonRepository {
+  private repository: PokemonDTO[]
+
+  constructor (repository: PokemonDTO[]) {
+    this.repository = repository
+  }
+
+  async add (pokemon: PokemonDTO): Promise<PokemonDTO> {
+    const exists = await this.exists(pokemon)
+
+    if (!exists) {
+      this.repository.push(pokemon)
+    }
+
+    return pokemon
+  }
+
+  async findPokemonByName (name: string): Promise<PokemonDTO> {
+    const pokemon = this.repository.find(pokemon => pokemon.name === name)
+
+    return pokemon || null
+  }
+
+  async findAllPokemons (): Promise<PokemonDTO[]> {
+    return this.repository
+  }
+
+  async exists (pokemon: PokemonDTO): Promise<boolean> {
+    if (await this.findPokemonByName(pokemon.name) === null) {
+      return false
+    }
+
+    return true
+  }
+}
