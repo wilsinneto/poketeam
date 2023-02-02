c5386110bea72fb35a688e022852ed09f570710f
wilson Neto <wngaspar10@gmail.com>
2023-02-02 10:01:52 -0300

    test: added name object value

---
+++ test/entities/name-validation.spec.ts
@@ -0,0 +1,28 @@
+import { Name } from '@/entities/pokemon'
+
+describe('Name validation', () => {
+  test('should not accept null strings', () => {
+    const name: null = null
+    expect(Name.validate(name)).toBeFalsy()
+  })
+
+  test('should not accept empty strings', () => {
+    const name: string = ''
+    expect(Name.validate(name)).toBeFalsy()
+  })
+
+  test('should not accept name less than 2 chars', () => {
+    const name: string = 'l'
+    expect(Name.validate(name)).toBeFalsy()
+  })
+
+  test('should not accept name larger than 257 chars', () => {
+    const name: string = 'l'.repeat(257)
+    expect(Name.validate(name)).toBeFalsy()
+  })
+
+  test('should accept valid name', () => {
+    const name: string = 'One name'
+    expect(Name.validate(name)).toBeTruthy()
+  })
+})
