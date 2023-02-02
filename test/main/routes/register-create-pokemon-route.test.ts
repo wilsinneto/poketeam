00761a446516a440e1c1fe9f2b6fce106444e6a3
wilson Neto <wngaspar10@gmail.com>
2023-02-02 14:59:06 -0300

    test: register create pokemon route

---
+++ test/main/routes/register-create-pokemon-route.test.ts
@@ -0,0 +1,21 @@
+import request from 'supertest'
+
+import app from '@/main/config/app'
+
+describe('Register create pokemon route', () => {
+  test('should return an account on success', async () => {
+    app.post('/test_cors', (req, res) => {
+      res.send()
+    })
+    await request(app)
+      .post('/api/create-pokemon')
+      .send({
+        id: '',
+        teamId: '',
+        name: 'Any name',
+        imageUrl: '',
+        specie: 'any_specie',
+      })
+      .expect(201)
+  })
+})
