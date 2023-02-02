aeb548e0fc4db27510ec8cc743c6000c1a31957c
wilson Neto <wngaspar10@gmail.com>
2023-02-02 11:42:33 -0300

    test: user in memory pokemon repository

---
+++ test/use-cases/pokemon/repository/in-memory-pokemon-repository.spec.ts
@@ -0,0 +1,36 @@
+import { PokemonDTO } from '@/entities/pokemon'
+import { InMemoryPokemonRepository } from '@/use-cases/pokemon/repository'
+
+describe('In memory Pokemon repository', () => {
+  test('should return null if pokemon is not found', async () => {
+    const pokemons: PokemonDTO[] = []
+    const sut = new InMemoryPokemonRepository(pokemons)
+    const pokemon = await sut.findPokemonByName('any')
+    expect(pokemon).toBeNull()
+  })
+
+  test('should return pokemon if it is found in the repository', async () => {
+    const pokemons: PokemonDTO[] = []
+    const name = 'any_name'
+    const imageUrl = 'http://image.png'
+    const specie = 'abc'
+    const sut = new InMemoryPokemonRepository(pokemons)
+
+    await sut.add({ id: '', teamId: '', name, imageUrl, specie })
+    const pokemon = await sut.findPokemonByName('any_name')
+
+    expect(pokemon.name).toBe('any_name')
+  })
+
+  test('should return all pokemons in the repository', async () => {
+    const pokemons: PokemonDTO[] = [
+      { id: '', teamId: '', name: 'any_name', specie: 'any', imageUrl: 'http://image.png' },
+      { id: '', teamId: '', name: 'second_name', specie: 'second', imageUrl: 'http://image2.png' }
+    ]
+
+    const sut = new InMemoryPokemonRepository(pokemons)
+    const returnedPokemons = await sut.findAllPokemons()
+
+    expect(returnedPokemons.length).toBe(2)
+  })
+})
