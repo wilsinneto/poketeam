d0e19e6fd76ef2d7711e2c33e4d34cf7daca9151
wilson Neto <wngaspar10@gmail.com>
2023-02-02 14:39:21 -0300

    feat: added content type config

--- src/main/config/middleware.ts
+++ src/main/config/middleware.ts
@@ -1,8 +1,8 @@
-import { bodyParser } from '@/main/middleware'
-import { cors } from '@/main/middleware/cors'
+import { bodyParser, contentType, cors } from '@/main/middleware'
 import { Express } from 'express'
 
 export default (app: Express): void => {
   app.use(bodyParser)
   app.use(cors)
+  app.use(contentType)
 }
