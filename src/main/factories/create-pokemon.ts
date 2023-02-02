41210ec6caed45a0f8a20a1cd944f005e2b1afd1
wilson Neto <wngaspar10@gmail.com>
2023-02-02 16:01:12 -0300

    refactor: create tables in memory

--- src/main/factories/create-pokemon.ts
+++ src/main/factories/create-pokemon.ts
@@ -1,9 +1,10 @@
 import { CreatePokemon } from '@/use-cases/pokemon'
 import { InMemoryPokemonRepository } from '@/use-cases/pokemon/repository'
 import { CreatePokemonController } from '@/web-controller/create-pokemon-controller'
+import { pokemons } from '../database/in-memory'
 
 export const makeCreatePokemonController = (): CreatePokemonController => {
-  const inMemoryPokemonRepository = new InMemoryPokemonRepository([])
+  const inMemoryPokemonRepository = new InMemoryPokemonRepository(pokemons)
   const createPokemonUseCase = new CreatePokemon(inMemoryPokemonRepository)
   const createPokemonController = new CreatePokemonController(createPokemonUseCase)
 
