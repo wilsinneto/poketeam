843cd7b66c6e76ae3f6709dcfcd5e63e812044d0
wilson Neto <wngaspar10@gmail.com>
2023-02-03 17:12:18 -0300

    feat: updated pokemon team repository

--- src/use-cases/pokemon-team/ports/pokemon-team-repository.ts
+++ src/use-cases/pokemon-team/ports/pokemon-team-repository.ts
@@ -4,8 +4,10 @@ import { PokemonTeamDTO } from '@/entities/pokemon-team'
 export interface PokemonTeamRepository {
   add(pokemon: PokemonTeamDTO): Promise<PokemonTeamDTO>
   findPokemonTeamByName(name: string): Promise<PokemonTeamDTO>
+  findPokemonTeamById(id: string): Promise<boolean>
   findAllPokemonTeam(): Promise<PokemonTeamDTO[]>
   exists(pokemon: PokemonTeamDTO): Promise<boolean>
   isMoreSix(pokemons: PokemonDTO[]): Promise<boolean>
   specieAlreadyExist(pokemons: PokemonDTO[]): Promise<boolean>
+  remove(id: string): Promise<boolean>
 }
