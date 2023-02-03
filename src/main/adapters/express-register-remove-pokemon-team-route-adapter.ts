a49112ab453b9d56cf74eb9f5b841b272e1a321e
wilson Neto <wngaspar10@gmail.com>
2023-02-03 18:49:42 -0300

    feat: create remove pokemon team adapter

---
+++ src/main/adapters/express-register-remove-pokemon-team-route-adapter.ts
@@ -0,0 +1,15 @@
+import { HttpRequest } from '@/web-controller/ports'
+import { RemovePokemonTeamController } from '@/web-controller/remove-pokemon-team-controller'
+import { Request, Response } from 'express'
+
+export const adaptRegisterRemovePokemonTeamRoute = (controller: RemovePokemonTeamController) => {
+  return async (request: Request, response: Response) => {
+    const httpRequest: HttpRequest = {
+      body: request.body
+    }
+
+    const httpResponse = await controller.handle(httpRequest)
+
+    response.status(httpResponse.statusCode).json(httpResponse.body)
+  }
+}
