36fa4129efb660abfc8d5672083c83334c4be334
wilson Neto <wngaspar10@gmail.com>
2023-02-03 12:09:36 -0300

    test: added is more six validation and specie already exist validation

--- test/use-cases/pokemon-team/repository/in-memory-pokemon-team-repository.spec.ts
+++ test/use-cases/pokemon-team/repository/in-memory-pokemon-team-repository.spec.ts
@@ -2,6 +2,58 @@ import { PokemonTeamDTO } from '@/entities/pokemon-team'
 import { InMemoryPokemonTeamRepository } from '@/use-cases/pokemon-team/repository'
 
 describe('In memory Pokemon Team repository', () => {
+  test('should return false if specie not exist', async () => {
+    const pokemonTeam: PokemonTeamDTO[] = []
+    const sut = new InMemoryPokemonTeamRepository(pokemonTeam)
+    const pokemons = [
+      { id: '', teamId: '', imageUrl: '', name: 'any', specie: 'any specie' },
+      { id: '', teamId: '', imageUrl: '', name: 'any two', specie: 'any specie two' }
+    ]
+
+    const pokemon = await sut.specieAlreadyExist(pokemons)
+    expect(pokemon).toBeFalsy()
+  })
+
+  test('should return true if specie exist', async () => {
+    const pokemonTeam: PokemonTeamDTO[] = []
+    const sut = new InMemoryPokemonTeamRepository(pokemonTeam)
+    const pokemons = [
+      { id: '', teamId: '', imageUrl: '', name: 'any', specie: 'any specie' },
+      { id: '', teamId: '', imageUrl: '', name: 'any two', specie: 'any specie' }
+    ]
+
+    const pokemon = await sut.specieAlreadyExist(pokemons)
+    expect(pokemon).toBeTruthy()
+  })
+
+  test('should return false if pokemon quantity less than or equal to 6', async () => {
+    const pokemonTeam: PokemonTeamDTO[] = []
+    const sut = new InMemoryPokemonTeamRepository(pokemonTeam)
+    const pokemons = [
+      { id: '', teamId: '', imageUrl: '', name: 'any', specie: 'any specie' }
+    ]
+
+    const pokemon = await sut.isMoreSix(pokemons)
+    expect(pokemon).toBeFalsy()
+  })
+
+  test('should return true if greater than 6', async () => {
+    const pokemonTeam: PokemonTeamDTO[] = []
+    const sut = new InMemoryPokemonTeamRepository(pokemonTeam)
+    const pokemons = [
+      { id: '', teamId: '', imageUrl: '', name: 'any', specie: 'any specie' },
+      { id: '', teamId: '', imageUrl: '', name: 'any two', specie: 'any specie' },
+      { id: '', teamId: '', imageUrl: '', name: 'any', specie: 'any specie' },
+      { id: '', teamId: '', imageUrl: '', name: 'any two', specie: 'any specie' },
+      { id: '', teamId: '', imageUrl: '', name: 'any', specie: 'any specie' },
+      { id: '', teamId: '', imageUrl: '', name: 'any two', specie: 'any specie' },
+      { id: '', teamId: '', imageUrl: '', name: 'any', specie: 'any specie' },
+    ]
+
+    const pokemon = await sut.specieAlreadyExist(pokemons)
+    expect(pokemon).toBeTruthy()
+  })
+
   test('should return null if pokemon team is not found', async () => {
     const pokemonTeam: PokemonTeamDTO[] = []
     const sut = new InMemoryPokemonTeamRepository(pokemonTeam)
