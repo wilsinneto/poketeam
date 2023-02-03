d9aa5a6383e038bf6c4ffa2bec19cbcaf83dab23
wilson Neto <wngaspar10@gmail.com>
2023-02-03 16:00:14 -0300

    test: find all pokemon team use case

---
+++ test/use-cases/pokemon-team/find-all-pokemon-team.spec.ts
@@ -0,0 +1,33 @@
+import { PokemonTeamDTO } from '@/entities/pokemon-team'
+import { FindAllPokemonTeam } from '@/use-cases/pokemon-team/find-all-pokemon-team'
+import { PokemonTeamRepository } from '@/use-cases/pokemon-team/ports'
+import { InMemoryPokemonTeamRepository } from '@/use-cases/pokemon-team/repository'
+
+describe('Find all pokemon team use case', () => {
+  test('should find empty pokemon team list', async () => {
+    const pokemonTeam: PokemonTeamDTO[] = []
+    const repository: PokemonTeamRepository = new InMemoryPokemonTeamRepository(pokemonTeam)
+    const useCase: FindAllPokemonTeam = new FindAllPokemonTeam(repository)
+
+    const response = await useCase.perform()
+    const pokemonList = await repository.findAllPokemonTeam()
+
+    expect(pokemonList.length).toBe(0)
+    expect(response.value.length).toBe(0)
+  })
+
+  test('should find pokemon list with 2 elements', async () => {
+    const pokemonTeam: PokemonTeamDTO[] = []
+    const repository: PokemonTeamRepository = new InMemoryPokemonTeamRepository(pokemonTeam)
+    const useCase: FindAllPokemonTeam = new FindAllPokemonTeam(repository)
+    
+    await repository.add({ name: 'any_name one', pokemons: [] })
+    await repository.add({ name: 'any_name two', pokemons: [] })
+
+    const response = await useCase.perform()
+    const pokemonList = await repository.findAllPokemonTeam()
+
+    expect(pokemonList.length).toBe(2)
+    expect(response.value.length).toBe(2)
+  })
+})
