6ec436effc5f2abbfd26527962d665a1383c438f
wilson Neto <wngaspar10@gmail.com>
2023-02-03 15:04:03 -0300

    feat: added find all pokemons controller

---
+++ src/web-controller/find-all-pokemons-controller.ts
@@ -0,0 +1,21 @@
+import { UseCase } from '@/use-cases/ports'
+import { HttpResponse } from '@/web-controller/ports'
+import { ok, serverError } from '@/web-controller/util/http-helper'
+
+export class FindAllPokemonsController {
+  private readonly useCase: UseCase
+
+  constructor (useCase: UseCase) {
+    this.useCase = useCase
+  }
+
+  public async handle (): Promise<HttpResponse> {
+    try {
+      const response = await this.useCase.perform()
+
+      return ok(response.value)
+    } catch (error) {
+      return serverError(error)
+    }
+  }
+}
