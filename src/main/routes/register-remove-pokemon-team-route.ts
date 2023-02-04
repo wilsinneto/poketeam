10ca4a8dab007c778db791e785c85f2ebb290c53
wilson Neto <wngaspar10@gmail.com>
2023-02-04 09:32:22 -0300

    feat: added remove pokemon team route

---
+++ src/main/routes/register-remove-pokemon-team-route.ts
@@ -0,0 +1,7 @@
+import { adaptRegisterRemovePokemonTeamRoute } from '@/main/adapters/express-register-remove-pokemon-team-route-adapter'
+import { makeRemovePokemonTeamController } from '@/main/factories/remove-pokemon-team'
+import { Router } from 'express'
+
+export default (router: Router): void => {
+  router.post('/remove-pokemon-team', adaptRegisterRemovePokemonTeamRoute(makeRemovePokemonTeamController()))
+}
