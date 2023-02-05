84c670526adc93cfcb36f59c06b809ef63b3f3a5
wilson Neto <wngaspar10@gmail.com>
2023-02-05 10:49:37 -0300

    fix: updated pokemons table from pokemon team table

--- src/main/factories/find-all-pokemon-team.ts
+++ src/main/factories/find-all-pokemon-team.ts
@@ -1,10 +1,10 @@
-import { pokemons } from '@/main/database/in-memory'
+import { pokemonTeam } from '@/main/database/in-memory'
 import { FindAllPokemonTeam } from '@/use-cases/pokemon-team/find-all-pokemon-team'
 import { InMemoryPokemonTeamRepository } from '@/use-cases/pokemon-team/repository'
 import { FindAllPokemonTeamController } from '@/web-controller/find-all-pokemon-team-controller'
 
 export const makeFindAllPokemonTeamController = (): FindAllPokemonTeamController => {
-  const inMemoryPokemonTeamRepository = new InMemoryPokemonTeamRepository(pokemons)
+  const inMemoryPokemonTeamRepository = new InMemoryPokemonTeamRepository(pokemonTeam)
   const findAllPokemonTeamUseCase = new FindAllPokemonTeam(inMemoryPokemonTeamRepository)
   const findAllPokemonTeamController = new FindAllPokemonTeamController(findAllPokemonTeamUseCase)
 
