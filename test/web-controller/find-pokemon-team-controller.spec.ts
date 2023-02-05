eabbd2fca31c5676bdfd84a26162b63100659cea
wilson Neto <wngaspar10@gmail.com>
2023-02-05 13:26:12 -0300

    test: create find pokemon team controller

---
+++ test/web-controller/find-pokemon-team-controller.spec.ts
@@ -0,0 +1,52 @@
+import { PokemonTeamDTO } from '@/entities/pokemon-team'
+import { FindPokemonTeam } from '@/use-cases/pokemon-team/find-pokemon-team'
+import { PokemonTeamRepository } from '@/use-cases/pokemon-team/ports'
+import { InMemoryPokemonTeamRepository } from '@/use-cases/pokemon-team/repository'
+import { UseCase } from '@/use-cases/ports'
+import { FindPokemonTeamController } from '@/web-controller/find-pokemon-team-controller'
+import { HttpRequest, HttpResponse } from '@/web-controller/ports'
+
+describe('Find pokemon team web controller', () => {
+  const pokemonTeam: PokemonTeamDTO[] = []
+  const repository: PokemonTeamRepository = new InMemoryPokemonTeamRepository(pokemonTeam)
+  const findPokemonTeamUseCase: UseCase = new FindPokemonTeam(repository)
+  const controller: FindPokemonTeamController = new FindPokemonTeamController(findPokemonTeamUseCase)
+
+  class ErrorThrowingUseCaseStub implements UseCase {
+    perform (request: any): Promise<void> {
+      throw Error()
+    }
+  }
+
+  const errorThrowingUseCaseStub: UseCase = new ErrorThrowingUseCaseStub()
+
+  test('should return status code 200 when request contains valid pokemon team data', async () => {
+    const findPokemonRequest: HttpRequest = {
+      body: {},
+      params: {
+        name: 'Any name'
+      }
+    }
+
+    await repository.add({ name: 'Any name', pokemons: [] })
+
+    const response: HttpResponse = await controller.handle(findPokemonRequest)
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body).toEqual({ id: response.body.id, name: 'Any name', pokemons: [] })
+  })
+
+  test('should return status code 500 when server raises', async () => {
+    const request: HttpRequest = {
+      body: {
+        name: 'Any another',
+      }
+    }
+
+    const controller: FindPokemonTeamController = new FindPokemonTeamController(errorThrowingUseCaseStub)
+    const response: HttpResponse = await controller.handle(request)
+
+    expect(response.statusCode).toEqual(500)
+    expect(response.body).toBeInstanceOf(Error)
+  })
+})
